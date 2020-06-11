38ccb0d1b171b6280f3bcb8ff15e33dc8a915f62
wfp <wfp@MacBook-Pro.local>
2020-06-11 10:44:32 +0800

    fix: 运行 bug

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # wangEditor next demo
 
+用于安装 npm 包，功能测试
+
+## 安装
+
+### 安装线上 npm 包
+
 `npm i @wangeditor-team/we-next --save` ，确保安装的是**最新**版本
 
-随便修改代码
+### 安装本地 npm 包
 
-运行 `npm run dev`
+进入编辑器源码目录，运行 `npm link` ，会得到一个本地的 npm 包地址，如 `/Users/xxx/.nvm/versions/node/v14.3.0/lib/node_modules/@wangeditor-team/we-next` ，将其拷贝下来。
+
+进入该项目目录，运行 `npm install <刚才拷贝的地址>` ，即可安装完成
+
+## 修改代码
+
+可按自己需求，修改、添加代码
+
+## 运行
+
+`npm run dev`
--- webpack.config.js
+++ webpack.config.js
@@ -7,23 +7,36 @@ const path = require('path')
 // const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const srcPath = path.join(__dirname, 'src')
+const distPath = path.join(__dirname, 'dist')
+
 module.exports = {
     mode: 'development',
-    entry: path.join(__dirname, 'src', 'index.js'),
+    entry: path.join(srcPath, 'index.js'),
     output: {
         filename: 'main.js',
-        path: path.join(__dirname, 'dist')
+        path: distPath
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'index.html'),
+            template: path.join(srcPath, 'index.html'),
             filename: 'index.html'
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: ['babel-loader'],
+                include: srcPath,
+                exclude: /node_modules/
+            },
+        ]
+    },
     devServer: {
         port: 3001,
         progress: true,
-        contentBase: path.join(__dirname, 'dist'),
+        contentBase: distPath,
         open: true,
         compress: true
     }
